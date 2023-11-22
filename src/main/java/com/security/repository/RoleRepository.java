a18a73ba6bd4ecd0473925b95cff5c934ea679f0
TrickAndTarck <nahushzilpilwar@gmail.com>
2023-11-22 10:53:31 -0500

    Project commit

---
+++ src/main/java/com/security/repository/RoleRepository.java
@@ -0,0 +1,18 @@
+package com.security.repository;
+
+
+
+
+import com.security.models.ERole;
+import com.security.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+@Repository
+
+public interface RoleRepository extends JpaRepository<ERole, Long> {
+  Optional<Role> findByName(ERole name);
+}
